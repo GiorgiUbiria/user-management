4d962afa5fba09eec47e2dfdccadb2422fcc3484
Giorgi Ubiria <ubiriagiorgi8@gmail.com>
2022-10-25 16:51:07 +0400

    starting project

---
+++ src/models/userSchema.ts
@@ -0,0 +1,34 @@
+import mongoose, { Schema, Document } from "mongoose"
+
+
+export interface IUser extends Document {
+  email: String
+  firstName: String
+  lastName: String
+}
+
+const UserSchema: Schema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  login: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+})
+
+export default mongoose.model<IUser>("User", UserSchema)
