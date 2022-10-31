3565d01650fa64df45ac06d0d426e2e1a17ab7fb
Giorgi Ubiria <ubiriagiorgi8@gmail.com>
2022-10-31 15:38:00 +0400

    Changed addUser with createUser. Used interface and different approach. Will Soon use other github repo as reference to change everything here

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction, Router } from "express";
+/* import { Request, Response, NextFunction, Router } from "express";
 import UserSchema from "../models/userSchema";
 
 const getUsers = async (
@@ -27,6 +27,11 @@ const addUser = async (
   const userEmail: string = req.body.email;
   const userRole: string = req.body.role;
 
+  // tslint:disable-next-line:no-console
+  console.log(userUserName);
+  // tslint:disable-next-line:no-console
+  console.log(userEmail);
+
   const user = new UserSchema({
     email: userEmail,
     password: userPassword,
@@ -34,8 +39,10 @@ const addUser = async (
     role: userRole,
   });
 
-  try {
+ try {
     const result = await user.save();
+    // tslint:disable-next-line:no-console
+    console.log(result);
     res.redirect("/");
   } catch (error) {
     // tslint:disable-next-line:no-console
@@ -44,3 +51,35 @@ const addUser = async (
 };
 
 export { getUsers, addUser };
+ */
+
+import UserSchema, { IUser } from "../models/user.model";
+
+interface ICreateUserInput {
+  email: IUser["email"];
+  username: IUser["username"];
+  password: IUser["password"];
+  role: IUser["role"];
+}
+
+async function CreateUser({
+  email,
+  username,
+  password,
+  role,
+}: ICreateUserInput): Promise<IUser> {
+  return UserSchema.create({
+    email,
+    username,
+    password,
+    role,
+  })
+    .then((data: IUser) => {
+      return data;
+    })
+    .catch((error: Error) => {
+      throw error;
+    });
+}
+
+export default { CreateUser };
--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,10 +1,23 @@
 import { Request, Response, NextFunction, Router } from "express";
-import { getUsers, addUser } from "../controllers/userController";
+/* import { getUsers, addUser } from "../controllers/userController"; */
+import  UserController  from "../controllers/userController";
 
 const UsersRouter: Router = Router();
 
-UsersRouter.get("/", getUsers);
+/* UsersRouter.get("/", getUsers);
 
-UsersRouter.post("/add-user", addUser);
+UsersRouter.post("/add-user", addUser); */
+
+UsersRouter.post("/add-user", async (req: Request, res: Response) => {
+    const user = await UserController.CreateUser({
+      email: req.body.firstName,
+      username: req.body.lastName,
+      password: req.body.email,
+      role: req.body.role,
+    });
+
+    // tslint:disable-next-line:no-console
+    console.log(user);
+});
 
 export { UsersRouter };
